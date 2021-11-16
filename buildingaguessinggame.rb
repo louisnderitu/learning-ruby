7579376ac5fbc4715992fe7fa765cfb133b1dcfa
louisnderitu <louismaina444@gmail.com>
2021-11-16 15:23:01 +0300

    loops

--- buildingaguessinggame.rb
+++ buildingaguessinggame.rb
@@ -1,10 +1,23 @@
 #BUILDING A GUESSING GAME
 secret_word = "luigi"
 guess = ""
+guess_count = 0
+guess_count = 1
+guess_limit = 4
+out_of_guesses = false
 
-while guess != secret_word
-  puts "Enter guess: "
-  guess = gets.chomp()
+while guess != secret_word and !out_of_guesses
+  if guess_count < guess_limit
+    puts "Enter guess: "
+    guess = gets.chomp()
+    guess_count += 1
+  else
+  out_of_guesses = true
+  end
 end
 
-puts "You Won!"
+if out_of_guesses
+  puts "You lose"
+else
+  puts "You Won"
+end
